3e577401c42e5aa37bb0a20aefd5fa924e0c1f84
Puneet Singla <puneet.devs@gmail.com>
2022-07-04 19:08:00 +0530

    use cart trait in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    use UserAPIToken;
+    use UserAPIToken, UserCardProducts;
     /**
      * The attributes that are mass assignable.
      *
