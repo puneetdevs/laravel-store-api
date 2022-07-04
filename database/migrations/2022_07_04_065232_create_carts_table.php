9d9bab225a2bf353d96c2de05671324e7aabf24c
Puneet Singla <puneet.devs@gmail.com>
2022-07-04 18:59:59 +0530

    cart migratio updated

---
+++ database/migrations/2022_07_04_065232_create_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->nullable()->references('id')->on('users');
+            $table->bigInteger('product_id')->unsigned()->nullable()->references('id')->on('products');
+            $table->integer('quantity')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
